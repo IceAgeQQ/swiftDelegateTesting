e97d2d96bec7d46074c96af1ef14bdf060a73740
IceAgeQQ <d830300042@gmail.com>
2016-02-03 18:40:16 +0800

    first commit

---
+++ swiftSQLite/TestViewController.swift
@@ -0,0 +1,106 @@
+//
+//  TestViewController.swift
+//  swiftSQLite
+//
+//  Created by Chao Xu on 2/1/16.
+//  Copyright © 2016 Chao Xu. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBOutlet weak var phoneLabel: UILabel!
+    
+    @IBOutlet weak var trydelegateLabel: UILabel!
+    
+    var name: String?
+    var address: String?
+    var phone: String?
+    var trydelegate: String?
+     var databasePath = NSString()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let VC = storyboard.instantiateViewControllerWithIdentifier("viewcontroller") as! ViewController
+        VC.delegate = self
+        
+        let filemgr = NSFileManager.defaultManager()
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let docsDir = dirPaths[0]
+        databasePath = docsDir.stringByAppendingPathComponent("contacts.db")
+        //If the file does not yet exist the code creates the database by creating an FMDatabase instance initialized with the database file path. If the database creation is successful it is then opened via a call to the open method of the new database instance.
+        if !filemgr.fileExistsAtPath(databasePath as String) {
+            let contactDB = FMDatabase(path: databasePath as String)
+            if contactDB == nil {
+                print("Error: \(contactDB.lastErrorMessage())")
+            }
+            if contactDB.open() {
+                let sql_stmt = "CREATE TABLE IF NOT EXISTS CONTACTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ADDRESS TEXT, PHONE TEXT)"
+                if !contactDB.executeStatements(sql_stmt){
+                    print("Error: \(contactDB.lastErrorMessage())")
+                }
+                contactDB.close()
+            }else {
+                print("Error: \(contactDB.lastErrorMessage())")
+            }
+        }
+
+    }
+
+    
+    override func viewWillAppear(animated: Bool) {
+        addressLabel.text = address!
+        phoneLabel.text = phone!
+        trydelegateLabel.text = trydelegate
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loadDataFromSQLite(sender: AnyObject) {
+        let contactDB = FMDatabase(path: databasePath as String)
+        
+        if contactDB.open() {
+            let querySQL = "SELECT name FROM CONTACTS WHERE name = 'go'"
+            let results:FMResultSet? = contactDB.executeQuery(querySQL, withArgumentsInArray: nil)
+            
+            if results?.next() == true {
+                nameLabel.text = results!.stringForColumn("name")
+                
+            }else {
+                addressLabel.text = "hello"
+                phoneLabel.text = "word"
+            }
+            contactDB.close()
+        }else {
+            print("Error: \(contactDB.lastErrorMessage())")
+        }
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension TestViewController:viewControllerDelegate {
+    func viewcontrollerdelegate(setviewcontrollerdelegate: ViewController) {
+        self.trydelegate = setviewcontrollerdelegate.address.text!
+    }
+}
---
+++ swiftSQLite/ViewController.swift
@@ -0,0 +1,163 @@
+//
+//  ViewController.swift
+//  swiftSQLite
+//
+//  Created by Chao Xu on 1/29/16.
+//  Copyright © 2016 Chao Xu. All rights reserved.
+//
+
+import UIKit
+
+protocol viewControllerDelegate: class {
+    func viewcontrollerdelegate(setviewcontrollerdelegate: ViewController)
+}
+
+class ViewController: UIViewController ,UITextFieldDelegate {
+    weak var delegate: viewControllerDelegate?
+
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var address: UITextField!
+    @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var status: UILabel!
+    
+    var databasePath = NSString()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //Creates an NSFileManager instance and subsequently uses it to detect if the database file already exists.
+        let filemgr = NSFileManager.defaultManager()
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let docsDir = dirPaths[0]
+        databasePath = docsDir.stringByAppendingPathComponent("contacts.db")
+//        print(databasePath)
+        //If the file does not yet exist the code creates the database by creating an FMDatabase instance initialized with the database file path. If the database creation is successful it is then opened via a call to the open method of the new database instance.
+        if !filemgr.fileExistsAtPath(databasePath as String) {
+            let contactDB = FMDatabase(path: databasePath as String)
+            if contactDB == nil {
+                print("Error: \(contactDB.lastErrorMessage())")
+            }
+            if contactDB.open() {
+                let sql_stmt = "CREATE TABLE IF NOT EXISTS CONTACTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ADDRESS TEXT, PHONE TEXT)"
+                if !contactDB.executeStatements(sql_stmt){
+                    print("Error: \(contactDB.lastErrorMessage())")
+                }
+                contactDB.close()
+            }else {
+                print("Error: \(contactDB.lastErrorMessage())")
+            }
+        }
+    }
+
+    @IBAction func saveData(sender: AnyObject) {
+        if ((name.text?.isEmpty ) != false || (address.text?.isEmpty) != false || (phone.text?.isEmpty != false)) {
+            let alertController = UIAlertController(title: "Error", message: "Please Fill Up Every Textfield", preferredStyle: .Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }else{
+        let contactDB = FMDatabase(path: databasePath as String)
+        
+        if contactDB.open() {
+    
+            let insertSQL = "INSERT INTO CONTACTS (name, address, phone) VALUES ('\(name.text!)', '\(address.text!)', '\(phone.text!)')"
+            
+            let result = contactDB.executeUpdate(insertSQL,
+                withArgumentsInArray: nil)
+            
+            if !result {
+                status.text = "Failed to add contact"
+                print("Error: \(contactDB.lastErrorMessage())")
+            } else {
+                status.text = "Contact Added"
+                name.text = ""
+                address.text = ""
+                phone.text = ""
+            }
+        } else {
+            print("Error: \(contactDB.lastErrorMessage())")
+        }
+        }
+    }
+    
+    @IBAction func findContact(sender: AnyObject) {
+        let contactDB = FMDatabase(path: databasePath as String)
+        
+        if contactDB.open() {
+            let querySQL = "SELECT address, phone FROM CONTACTS WHERE name = '\(name.text!)'"
+            let results:FMResultSet? = contactDB.executeQuery(querySQL, withArgumentsInArray: nil)
+            
+            if results?.next() == true {
+                address.text = results!.stringForColumn("address")
+                phone.text = results!.stringForColumn("phone")
+                status.text = "Record Found"
+            }else {
+                status.text = "Record not found"
+                address.text = ""
+                phone.text = ""
+            }
+            contactDB.close()
+        }else {
+            print("Error: \(contactDB.lastErrorMessage())")
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func nextViewandDelegate(sender: AnyObject) {
+        
+        self.delegate?.viewcontrollerdelegate(self)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showTestSegue" {
+            if let testViewController = segue.destinationViewController as? TestViewController {
+        
+                testViewController.address = address.text!
+                testViewController.phone = phone.text!
+                
+            }
+        }
+    }
+
+}
+
+extension String {
+    
+    func stringByAppendingPathComponent(path: String) -> String {
+        
+        let nsSt = self as NSString
+        
+        return nsSt.stringByAppendingPathComponent(path)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
